ba9f5a3d11385d9016ada1eeab343b84dc000374
Winter Alava <walava@espol.edu.ec>
2020-08-21 11:44:03 -0500

    Configuracion del activity registros, del gradle y sus dependencias

---
+++ app/src/main/java/com/amst/grupo1/RegistrosActivity.java
@@ -0,0 +1,14 @@
+package com.amst.grupo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+    }
+}
